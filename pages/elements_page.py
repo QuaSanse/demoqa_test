892a7ec82a9e45714c6a566ed104af3ca787f035
QuaSanse <quasanse@gmail.com>
2022-12-14 21:56:38 +0300

    Добавлена проверка TestWebTables, добавил поля в генератор, добавил локаторы для TestWebTables

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,12 +1,9 @@
 import time
 import random
 
-import requests
-from selenium.webdriver.common.by import By
-
 from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
-    WebTablesPageLocators, ButtonsPageLocators, LinksPageLocators
+    WebTablesPageLocators
 from pages.base_page import Base_page
 
 
@@ -117,102 +114,4 @@ class WebTablesPage(Base_page):
             self.element_is_visible(self.locators.DEPARTMENT_INPUT).send_keys(department)
             self.element_is_visible(self.locators.SUBMIT).click()
             count -= 1
-            return [first_name, last_name, str(age), email, str(salary), department]
-
-    def check_new_added_person(self):
-        person_list = self.elements_are_present(self.locators.FULL_PERSON_LIST)
-        data = []
-        for item in person_list:
-            data.append(item.text.splitlines())
-        return data
-
-    def search_some_person(self, key_word):
-        self.element_is_visible(self.locators.SEARCH_INPUT).send_keys(key_word)
-
-    def check_search_person(self):
-        delete_button = self.element_is_present(self.locators.DELETE_BUTTON)
-        row = delete_button.find_element("xpath", self.locators.ROW_PARENT)
-        return row.text.splitlines()
-
-    def update_person_info(self):
-        person_info = next(generated_person())
-        age = person_info.age
-        self.element_is_visible(self.locators.UPDATE_BUTTON).click()
-        self.element_is_visible(self.locators.AGE_INPUT).clear()
-        self.element_is_visible(self.locators.AGE_INPUT).send_keys(age)
-        self.element_is_visible(self.locators.SUBMIT).click()
-        return str(age)
-
-    def delete_person(self):
-        self.element_is_visible(self.locators.DELETE_BUTTON).click()
-
-    def check_deleted(self):
-        return self.element_is_present(self.locators.NO_ROWS_FOUND).text
-
-    def select_up_to_some_rows(self):
-        count = [5, 10, 20, 25, 50, 100]
-        data = []
-        for x in count:
-            count_row_button = self.element_is_present(self.locators.COUNT_ROW_LIST)
-            self.go_to_element(count_row_button)
-            count_row_button.click()
-            self.element_is_visible((By.CSS_SELECTOR, f'option[value="{x}"]')).click()
-            data.append(self.check_count_rows())
-        return data
-
-    def check_count_rows(self):
-        list_rows = self.elements_are_present(self.locators.FULL_PERSON_LIST)
-        return len(list_rows)
-
-
-class ButtonsPage(Base_page):
-    locators = ButtonsPageLocators()
-
-    result_dict = {}
-
-    def click_on_different_button(self) -> dict:
-        self.click_on_the_double_button()
-        self.click_on_the_right_button()
-        self.click_on_the_button()
-        return self.result_dict
-
-    def click_on_the_double_button(self):
-        self.action_double_click(self.element_is_visible(self.locators.DOUBLE_BUTTON))
-        result = self.check_clicked_on_the_button(self.locators.SUCCESS_DOUBLE)
-        self.result_dict.update({"double_click": result})
-
-    def click_on_the_right_button(self):
-        self.action_right_click(self.element_is_visible(self.locators.RIGHT_CLICK_BUTTON))
-        result = self.check_clicked_on_the_button(self.locators.SUCCESS_RIGHT)
-        self.result_dict.update({"right_click": result})
-
-    def click_on_the_button(self):
-        self.element_is_visible(self.locators.CLICK_ME_BUTTON).click()
-        result = self.check_clicked_on_the_button(self.locators.SUCCESS_CLICK_ME)
-        self.result_dict.update({"click": result})
-
-    def check_clicked_on_the_button(self, element):
-        return self.element_is_present(element).text
-
-
-class LinksPage(Base_page):
-    locators = LinksPageLocators()
-
-    def check_new_tab_simple_link(self):
-        simple_link = self.element_is_visible(self.locators.SIMPLE_LINK)
-        link_href = simple_link.get_attribute('href')
-        request = requests.get(link_href)
-        if request.status_code == 200:
-            simple_link.click()
-            self.driver.switch_to.window(self.driver.window_handles[1])
-            url = self.driver.current_url
-            return link_href, url
-        else:
-            return link_href, request.status_code
-
-    def check_broken_link(self, url):
-        request = requests.get(url)
-        if request.status_code == 200:
-            self.element_is_present(self.locators.BAD_REQUEST).click()
-        else:
-            return request.status_code
+            return person_info, first_name, last_name, email, age, salary, department
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,6 @@
-import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablesPage, ButtonsPage, LinksPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablesPage
 
 
 class TestElements:
@@ -59,64 +58,5 @@ class TestElements:
             url = 'https://demoqa.com/webtables'
             web_tables_page = WebTablesPage(driver, url)
             web_tables_page.open()
-            new_person = web_tables_page.add_new_person(2)
-            table_result = web_tables_page.check_new_added_person()
-            assert new_person in table_result, "the person was not found in the table"
-
-        def test_web_table_search_person(self, driver):
-            web_table_page = WebTablesPage(driver, 'https://demoqa.com/webtables')
-            web_table_page.open()
-            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
-            print(key_word)
-            web_table_page.search_some_person(key_word)
-            table_result = web_table_page.check_search_person()
-            assert key_word in table_result, "the person was not found in the table"
-
-        def test_web_table_update_person_info(self, driver):
-            web_table_page = WebTablesPage(driver, 'https://demoqa.com/webtables')
-            web_table_page.open()
-            lastname = web_table_page.add_new_person()[1]
-            web_table_page.search_some_person(lastname)
-            age = web_table_page.update_person_info()
-            row = web_table_page.check_search_person()
-            assert age in row, "the person card has not been changed"
-
-        def test_web_table_delete_person(self, driver):
-            web_table_page = WebTablesPage(driver, 'https://demoqa.com/webtables')
-            web_table_page.open()
-            email = web_table_page.add_new_person()[3]
-            web_table_page.search_some_person(email)
-            web_table_page.delete_person()
-            text = web_table_page.check_deleted()
-            assert text == "No rows found"
-
-        def test_web_table_change_count_row(self, driver):
-            web_table_page = WebTablesPage(driver, 'https://demoqa.com/webtables')
-            web_table_page.open()
-            count = web_table_page.select_up_to_some_rows()
-            assert count == [5, 10, 20, 25, 50, 100], \
-                'The number of rows in the table has not been changed or has changed incorrectly'
-
-    class TestButtonsPage:
-        """ Проверки для страницы Buttons """
-
-        def test_different_click_on_the_buttons(self, driver):
-            button_page = ButtonsPage(driver, 'https://demoqa.com/buttons')
-            button_page.open()
-            result = button_page.click_on_different_button()
-            assert result['double_click'] == "You have done a double click", "The double click button was not pressed"
-            assert result['right_click'] == "You have done a right click", "The right click button was not pressed"
-            assert result['click'] == "You have done a dynamic click", "The dynamic click button was not pressed"
-
-    class TestLinksPage:
-        def test_check_link(self, driver):
-            links_page = LinksPage(driver, 'https://demoqa.com/links')
-            links_page.open()
-            href_link, current_url = links_page.check_new_tab_simple_link()
-            assert href_link == current_url, "the link is broken or url is incorrect"
-
-        def test_broken_link(self, driver):
-            links_page = LinksPage(driver, 'https://demoqa.com/links')
-            links_page.open()
-            response_code = links_page.check_broken_link('https://demoqa.com/bad-request')
-            assert response_code == 400, "the link works or the status code in son 400"
+            web_tables_page.add_new_person(1)
+            time.sleep(5)
